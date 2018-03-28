a88f3e4144104c92c206990cc30941b38ce9f918
tkhrshhr <tkhrshhr@gmail.com>
2018-03-28 20:39:47 +0900

    train.sh

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ for kg in w f; do
     for weight in 0.01 0.001 0.0001; do
       for k in 1 4 10; do
         for l in 0.1 0.05; do
-          python train.py -c $kg -m $model -g $gpu -d $dim -w $weight -k $k -l $l 
+          python train.py -c $kg -m $model -g $gpu -d $dim -w $weight -k $k -l $l > log/0218_c$kg\m$model\g$gpu\d$dim\w$weight\k$k\l$l
         done
       done
     done
