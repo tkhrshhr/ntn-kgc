90bf80218a2ef20fd8821e892e3671abaef40d39
Ishihara Takahiro <takahiro-is@elm14.naist.jp>
2018-03-29 16:48:18 +0900

    find bug in train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
-model="c"
-gpu=-1
+model="t"
+gpu=2
 p=1
-for kg in w f; do
-  for dim in 1; do
+for kg in f; do
+  for dim in 100; do
     for k in 1 4; do
       for s in 2 10; do
         for weight in 0.001 0.0001 0.00001; do
           for l in 0.1 0.05 0.01; do
           python train.py \
-          -e 1 \
+          -e 300 \
           -m $model \
           -g $gpu \
           -c $kg \
